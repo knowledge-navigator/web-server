280cc80637ce889d0ef6277e69b3217cfe257af4
dainank <dainank@proton.me>
2023-02-21 11:23:07 +0100

    organization entity

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -2,3 +2,4 @@ pub mod user;
 pub mod course;
 pub mod pagination;
 pub mod info_chunks;
+pub mod organization;
---
+++ src/entities/organization.rs
@@ -0,0 +1,22 @@
+use super::{course::CourseId, user::UserId};
+use chrono::{DateTime, Utc}; // time conversions for local take place on frontend
+use serde::{Deserialize, Serialize};
+
+/// A collection of knowledge navigators, students and teachers, usually representative of a single school
+/// and its subject material.
+///
+/// An `Organization` can be created any `Teacher`, who is automatically also the moderator of said `Organization`.
+/// Moderator teachers can manipulate courses and members of the `Organization` as well as alleviate or remove any
+/// teacher to and from moderator status.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Organization {
+    pub id: OrganizationId,
+    pub name: String,
+    pub description: Option<String>,
+    pub utc_created: DateTime<Utc>,
+    pub moderators: Vec<UserId>,        // only UserType::Teacher
+    pub courses: Option<Vec<CourseId>>, // a knowledge navigator (e.g. Knowledge Navigator Y10)
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct OrganizationId(pub String);
