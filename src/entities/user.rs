a69f92c5972f395807fd160e884f4b66c3c9a4af
dainank <dainank@proton.me>
2023-02-21 10:03:32 +0100

    initial user entity

---
+++ src/entities/user.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct User {
+    pub id: UserId,
+    pub role: UserType,
+    pub first_name: String,
+    pub last_name: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct UserId(pub String);
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub enum UserType {
+    Student,
+    Teacher,
+}
